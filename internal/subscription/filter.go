e291ed1b462e4a10c8f41f8d73546bc617ea51bf
Sentsuki <52487960+Sentsuki@users.noreply.github.com>
2025-12-13 22:03:51 +0800

    minor fix

--- internal/subscription/filter.go
+++ internal/subscription/filter.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-var emojiRegex = regexp.MustCompile(`[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{1F700}-\x{1F77F}\x{1F780}-\x{1F7FF}\x{1F800}-\x{1F8FF}\x{1F900}-\x{1F9FF}\x{1FA00}-\x{1FA6F}\x{1FA70}-\x{1FAFF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{2300}-\x{23FF}]`)
+var emojiRegex = regexp.MustCompile(`\p{Emoji}`)
 
 // Filter provides node filtering functionality based on exclude keywords.
 // It can filter out nodes whose tags contain specified keywords.
@@ -24,7 +24,6 @@ func NewFilter(excludeKeywords []string) *Filter {
 }
 
 // FilterNodes filters out nodes that contain exclude keywords in their tags.
-// It performs case-insensitive matching and logs excluded nodes for debugging.
 func (f *Filter) FilterNodes(nodes []Node) []Node {
 	var filteredNodes []Node
 	excludedCount := 0
