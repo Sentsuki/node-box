5665980299f6fabfe8436447d9f6cef054206ce7
Sentsuki <52487960+Sentsuki@users.noreply.github.com>
2026-03-14 22:31:07 +0800

    chore: update go

--- cmd/node-box/main.go
+++ cmd/node-box/main.go
@@ -13,7 +13,7 @@ import (
 const (
 	defaultConfigPath = "config.json"
 	appName           = "node-box"
-	version           = "2.4.5"
+	version           = "2.4.6"
 )
 
 // printUsage 显示程序使用帮助信息
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module node-box
 
-go 1.26.1
+go 1.26
 
 require (
 	golang.org/x/exp v0.0.0-20260218203240-3dfff04db8fa
